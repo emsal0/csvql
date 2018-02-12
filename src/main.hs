f1d5fb8d051c425d439aba92de9cbf12fc8e043c
Emmanuel Sales <absolutezero2a03@gmail.com>
2018-02-11 17:55:36 -0800

    Begin CSV parsing functionality

--- src/main.hs
+++ src/main.hs
@@ -4,12 +4,21 @@ import Scanner
 import Data.Csv
 import qualified Data.ByteString.Lazy as BL
 
--- evaluate :: Query -> something idk
+evaluate :: Query -> IO BL.ByteString
 
+evaluate (Query selectExp (FromExp f) ) = do
+    csvData <- BL.readFile (f ++ ".csv")
+    return csvData
+
+evaluate (FilteredQuery selectExp fromExp whereExp) = return BL.empty
+
+main :: IO ()
 main = do
     s <- getContents
     let tokens = (alexScanTokens s)
     print tokens
     let parseTree = csvql tokens
     putStrLn ("parseTree: " ++ show(parseTree))
+    f <- evaluate parseTree
+    putStrLn ("query result: " ++ show(f))
     print "done"
