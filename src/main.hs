8903ed87cc52c65bdcc745ffd3bb4d53dbfaf36c
Em Sal <absolutezero2a03@gmail.com>
2018-02-22 16:40:05 -0800

    Get things into vector form

--- src/main.hs
+++ src/main.hs
@@ -1,16 +1,20 @@
 module Main where
 import Parser
 import Scanner
-import Data.Csv
 import qualified Data.ByteString.Lazy as BL
+import Data.ByteString (ByteString)
+import Data.Csv
+import qualified Data.Vector as V
 
-evaluate :: Query -> IO BL.ByteString
+evaluate :: Query -> IO String
 
 evaluate (Query selectExp (FromExp (SourceExp f)) ) = do
     csvData <- BL.readFile (f)
-    return csvData
+    case decode NoHeader csvData :: Either String (V.Vector (V.Vector ByteString)) of
+        Left err -> return err
+        Right v -> return (show v)
 
-evaluate (FilteredQuery selectExp fromExp whereExp) = return BL.empty
+evaluate (FilteredQuery selectExp fromExp whereExp) = return ""
 
 -- Where expressions
 evaluateWhere :: WhereExp -> Record -> Bool
@@ -30,5 +34,5 @@ main = do
     let parseTree = csvql tokens
     putStrLn ("parseTree: " ++ show(parseTree))
     f <- evaluate parseTree
-    putStrLn ("query result: " ++ show(f))
-    print "done"
+    putStrLn ("query result: " ++ f)
+    putStrLn "done"
